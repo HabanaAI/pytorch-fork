HabanaAI/pytorch-fork

commit bf1a893c797d8b31ccefd112bf1cfe2d4201b4fe

Update hip files (#9826)
Summary:
The goal of this PR is to update the hip files to reflect relevant changes in cuda source files.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/9826

Differential Revision: D9032840

Pulled By: bddppq

fbshipit-source-id: 504e55c46308eebfee3c9a7beea1f294fe03470f

caffe2/core/hip/context_hip.cc
@@ -4,8 +4,10 @@
 #include <string>
 #include <unordered_map>
 
+#include "caffe2/core/hip/THCCachingAllocator_hip.h"
 #include "cub/util_allocator.cuh"
 #include "caffe2/core/asan.h"
+#include "caffe2/core/blob_stats.h"
 #include "caffe2/core/hip/common_miopen.h"
 #include "caffe2/core/hip/context_hip.h"
 #include "caffe2/core/init.h"
@@ -60,7 +62,9 @@ thread_local ThreadLocalHIPObjects HIPContext::hip_objects_;
 HipMemoryPoolType g_hip_memory_pool_type;
 
 // For cub allocator
-unique_ptr<cub::CachingDeviceAllocator> g_cub_allocator;
+std::unique_ptr<cub::CachingDeviceAllocator> g_cub_allocator;
+
+std::unique_ptr<THCCachingAllocator> g_thc_allocator;
 // an unordered map that holds the map from the cuda memory pointer to the
 // device id that it is allocated from. This is used in the cuda memory pool
 // cases, where we need the device id to carry out the deletion.
@@ -180,6 +184,11 @@ static void Caffe2SetHIPMemoryPool()
         g_hip_memory_pool_type = HipMemoryPoolType::CUB;
         SetUpCub();
     }
+    else if(FLAGS_caffe2_hip_memory_pool == "thc")
+    {
+        g_hip_memory_pool_type = HipMemoryPoolType::THC;
+        g_thc_allocator.reset(new THCCachingAllocator());
+    }
     else
     {
         CAFFE_THROW("Unrecognized HIP memory pool type: ", FLAGS_caffe2_hip_memory_pool);
@@ -229,6 +238,21 @@ struct Caffe2HipInitializerHelper
         }
     }
 };
+
+struct TensorHIPStatGetter : BlobStatGetter {
+  size_t sizeBytes(const Blob& blob) const override {
+    const auto& tensor = blob.Get<TensorHIP>();
+    auto nbytes = tensor.nbytes();
+    if (nbytes > 0 && tensor.IsType<std::string>()) {
+      const auto* data = tensor.data<std::string>();
+      for (int i = 0; i < tensor.size(); ++i) {
+        nbytes += data[i].size();
+      }
+    }
+    return nbytes;
+  }
+};
+REGISTER_BLOB_STAT_GETTER(TensorHIP, TensorHIPStatGetter);
 } // namespace
 
 /**
@@ -339,6 +363,14 @@ std::pair<void*, MemoryDeleter> HIPStaticContext::New(size_t nbytes) const {
             g_size_map[ptr] = nbytes;
         }
         return {ptr, Delete};
+    case HipMemoryPoolType::THC:
+        HIP_ENFORCE(g_thc_allocator->Alloc(&ptr, nbytes, 0 /* stream */));
+        if (FLAGS_caffe2_gpu_memory_tracking)
+        {
+          g_size_map[ptr]                = nbytes;
+          g_hip_device_affiliation[ptr] = CaffeHipGetDevice();
+        }
+        return {ptr, Delete};
     }
     return {nullptr, Delete};
 }
@@ -388,6 +420,14 @@ void HIPStaticContext::Delete(void* ptr) {
         VLOG(2) << "CUB freeing pointer " << ptr << " on device " << it->second;
         HIP_ENFORCE(g_cub_allocator->DeviceFree(it->second, ptr));
         g_hip_device_affiliation.erase(it);
+        break;
+    }
+    case HipMemoryPoolType::THC: 
+    {
+        HIP_ENFORCE(g_thc_allocator->Free(ptr));
+        if (FLAGS_caffe2_gpu_memory_tracking) {
+          g_hip_device_affiliation.erase(g_hip_device_affiliation.find(ptr));
+        }
         break;
     }
     }

caffe2/core/THCCachingAllocator.cu
@@ -1,4 +1,4 @@
-#include "THCCachingAllocator.h"
+#include "caffe2/core/THCCachingAllocator_gpu.h"
 
 #include <deque>
 #include <map>

caffe2/core/context_gpu.cu
@@ -4,7 +4,7 @@
 #include <string>
 #include <unordered_map>
 
-#include "THCCachingAllocator.h"
+#include "caffe2/core/THCCachingAllocator_gpu.h"
 #include "cub/util_allocator.cuh"
 
 // Needed to be included first to check the CAFFE2_USE_CUDNN macros.

caffe2/core/hip/context_hip.h
@@ -19,6 +19,7 @@ namespace caffe2 {
 enum class HipMemoryPoolType {
   NONE = 0,
   CUB = 1,
+  THC = 2,
 };
 
 /**
@@ -51,7 +52,7 @@ class ThreadLocalHIPObjects {
 
   hipStream_t GetStream(int gpu, int stream_id) {
     vector<hipStream_t>& gpu_streams = hip_streams_[gpu];
-    if (gpu_streams.size() <= stream_id) {
+    if (gpu_streams.size() <= (unsigned)stream_id) {
       gpu_streams.resize(stream_id + 1, nullptr);
     }
     if (!gpu_streams[stream_id]) {
@@ -65,7 +66,7 @@ class ThreadLocalHIPObjects {
   rocblas_handle GetHandle(int gpu, int stream_id) {
     DeviceGuard guard(gpu);
     vector<rocblas_handle>& gpu_handles = rocblas_handles_[gpu];
-    if (gpu_handles.size() <= stream_id) {
+    if (gpu_handles.size() <= (unsigned)stream_id) {
       gpu_handles.resize(stream_id + 1, nullptr);
     }
     if (!gpu_handles[stream_id]) {
@@ -84,7 +85,7 @@ class ThreadLocalHIPObjects {
   miopenHandle_t GetMiopenHandle(int gpu, int stream_id) {
     DeviceGuard guard(gpu);
     vector<miopenHandle_t>& gpu_handles = miopen_handles_[gpu];
-    if (gpu_handles.size() <= stream_id) {
+    if (gpu_handles.size() <= (unsigned)stream_id) {
       gpu_handles.resize(stream_id + 1, nullptr);
     }
     if (!gpu_handles[stream_id]) {

caffe2/core/hip/net_async_dag_hip.cc
@@ -92,7 +92,7 @@ int AsyncDAGNet::stream(const DeviceOption& device_option)
     {
         int gpu_id = device_option.hip_gpu_id();
         CAFFE_ENFORCE_GE(gpu_id, 0, "Invalid gpu id: " + caffe2::to_string(gpu_id));
-        if(gpu_id >= stream_counters_.size())
+        if((unsigned)gpu_id >= stream_counters_.size())
         {
             stream_counters_.resize(gpu_id + 1, 0);
         }

caffe2/operators/hip/softmax_op_miopen.cc
@@ -44,6 +44,10 @@ class MIOpenSoftmaxOp final : public Operator<HIPContext> {
     const int D = X.size_from_dim(canonical_axis);
 
     Y->ResizeLike(X);
+    auto* Y_data = Y->template mutable_data<T>();
+    if (N == 0) {
+      return true;
+    }
     if (dims_ != X.dims()) {
       MIOPEN_ENFORCE(miopenSet4dTensorDescriptor(
           desc_, miopenTypeWrapper<T>::type, N, D, 1, 1));
@@ -56,7 +60,7 @@ class MIOpenSoftmaxOp final : public Operator<HIPContext> {
         X.template data<T>(),
         &beta_,
         desc_,
-        Y->template mutable_data<T>()));
+        Y_data));
     return true;
   }
 
@@ -99,6 +103,10 @@ class MIOpenSoftmaxGradientOp final : public Operator<HIPContext> {
 
     CHECK_EQ(Y.dims(), dY.dims());
     dX->ResizeLike(Y);
+    auto* dX_data = dX->template mutable_data<T>();
+    if (N == 0) {
+      return true;
+    }
     if (dims_ != Y.dims()) {
       MIOPEN_ENFORCE(miopenSet4dTensorDescriptor(
           desc_, miopenTypeWrapper<T>::type, N, D, 1, 1));
@@ -113,7 +121,7 @@ class MIOpenSoftmaxGradientOp final : public Operator<HIPContext> {
         dY.template data<T>(),
         &beta_,
         desc_,
-        dX->template mutable_data<T>()));
+        dX_data));
     return true;
   }
 

caffe2/operators/hip/spatial_batch_norm_op_miopen.cc
@@ -129,7 +129,7 @@ bool MIOpenSpatialBNOp::DoRunWithType() {
   CAFFE_ENFORCE_EQ(scale.dim32(0), C);
   CAFFE_ENFORCE_EQ(bias.dim32(0), C);
   // See if we need to reshape.
-  if (X.dims() != miopen_input_dims_) {
+  if (N > 0 && X.dims() != miopen_input_dims_) {
     VLOG(1) << "Setting descriptors.";
     miopen_input_dims_ = X.dims();
     vector<int> dims = {N, C, H, W, D};
@@ -153,6 +153,10 @@ bool MIOpenSpatialBNOp::DoRunWithType() {
 
     auto* Y = Output(OUTPUT);
     Y->ResizeLike(X);
+    T* Y_data = Y->template mutable_data<T>();
+    if (N == 0) {
+      return true;
+    }
     MIOPEN_ENFORCE(miopenBatchNormalizationForwardInference(
         miopen_wrapper_.inline_miopen_handle(),
         // Note: PERSISTENT not implemented for inference
@@ -162,7 +166,7 @@ bool MIOpenSpatialBNOp::DoRunWithType() {
         data_desc_,
         X.template data<T>(),
         data_desc_,
-        Y->template mutable_data<T>(),
+        Y_data,
         bn_param_desc_,
         const_cast<float*>(scale.template data<BNParamType>()),
         const_cast<float*>(bias.template data<BNParamType>()),
@@ -173,6 +177,7 @@ bool MIOpenSpatialBNOp::DoRunWithType() {
     // Run training mode.
     auto* Y = Output(OUTPUT);
     Y->ResizeLike(X);
+    T* Y_data = Y->template mutable_data<T>();
     // obtain running mean and running inv var, and see if we need to
     // initialize them.
     auto* running_mean = Output(RUNNING_MEAN);
@@ -212,6 +217,14 @@ bool MIOpenSpatialBNOp::DoRunWithType() {
     void* save_mean_data = save_mean->template mutable_data<BNParamType>();
     void* save_var_data = save_var->template mutable_data<BNParamType>();
 
+    if (N == 0) {
+      // set empty batch's mean / var output to zeros
+      math::Set<BNParamType, HIPContext>(
+          C, 0, (BNParamType*)save_mean_data, &context_);
+      math::Set<BNParamType, HIPContext>(
+          C, 0, (BNParamType*)save_var_data, &context_);
+      return true;
+    }
     MIOPEN_ENFORCE(miopenBatchNormalizationForwardTraining(
         miopen_wrapper_.inline_miopen_handle(),
         mode_,
@@ -220,7 +233,7 @@ bool MIOpenSpatialBNOp::DoRunWithType() {
         data_desc_,
         X.template data<T>(),
         data_desc_,
-        Y->template mutable_data<T>(),
+        Y_data,
         bn_param_desc_,
         const_cast<float*>(scale.template data<BNParamType>()),
         const_cast<float*>(bias.template data<BNParamType>()),
@@ -259,7 +272,7 @@ bool MIOpenSpatialBNGradientOp::DoRunWithType() {
   CAFFE_ENFORCE_EQ(scale.ndim(), 1);
   CAFFE_ENFORCE_EQ(scale.dim32(0), C);
   // See if we need to reshape.
-  if (X.dims() != miopen_input_dims_) {
+  if (N > 0 && X.dims() != miopen_input_dims_) {
     vector<int> dims = {N, C, H, W, D};
     vector<int> strides = {C * H * W * D, H * W * D, W * D, D, 1};
     MIOPEN_ENFORCE(miopenSet4dTensorDescriptor(
@@ -270,17 +283,28 @@ bool MIOpenSpatialBNGradientOp::DoRunWithType() {
   }
 
   auto* dX = Output(INPUT_GRAD);
+  dX->ResizeLike(X);
+  T* dX_data = dX->template mutable_data<T>();
   auto* dScale = Output(SCALE_GRAD);
   auto* dBias = Output(BIAS_GRAD);
   dX->ResizeLike(X);
   dScale->ResizeLike(scale);
   dBias->ResizeLike(scale);
+  auto* dScale_data = dScale->template mutable_data<BNParamType>();
+  auto* dBias_data = dBias->template mutable_data<BNParamType>();
 
   const auto& saved_mean = Input(SAVED_MEAN);
   const auto& saved_var = Input(SAVED_INV_VAR);
   const void* saved_mean_data = saved_mean.template data<BNParamType>();
   const void* saved_var_data = saved_var.template data<BNParamType>();
 
+if (N == 0) {
+    // set gradients to zeros
+    math::Set<BNParamType, HIPContext>(C, 0, dScale_data, &context_);
+    math::Set<BNParamType, HIPContext>(C, 0, dBias_data, &context_);
+    return true;
+  }
+  
   MIOPEN_ENFORCE(miopenBatchNormalizationBackward(
       miopen_wrapper_.inline_miopen_handle(),
       mode_,
@@ -293,11 +317,11 @@ bool MIOpenSpatialBNGradientOp::DoRunWithType() {
       data_desc_,
       dY.template data<T>(),
       data_desc_,
-      dX->template mutable_data<T>(),
+      dX_data,
       bn_param_desc_,
       scale.template data<BNParamType>(),
-      dScale->template mutable_data<BNParamType>(),
-      dBias->template mutable_data<BNParamType>(),
+      dScale_data,
+      dBias_data,
       epsilon_,
       saved_mean_data,
       saved_var_data));

commit 066ef4567ef2d8779d2f824b2d4ec91cad382a24

Fix BlobStatRegistry HIP BlobStatGetter registration issue (#9973)
Summary:
This was introduced in #9826 following the corresponding cuda file context_gpu.cu file, tests have passed in the PR, at that point master was 94439d7df. However during the long landing process, a new master commit aebf3b4 has come in that removed the `CAFFE_KNOWN_TYPE(Tensor<HIPContext>)` in context_hip.cc file, which then has broken the HIP BlobStatGetter, and we did NOT run tests again during merge and so when #9826 later landed to master the rocm tests start breaking.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/9973

Differential Revision: D9040671

Pulled By: bddppq

fbshipit-source-id: f3b16cabaf681fc0535ca733db0b48430868f922

caffe2/core/hip/context_hip.cc
@@ -239,20 +239,6 @@ struct Caffe2HipInitializerHelper
     }
 };
 
-struct TensorHIPStatGetter : BlobStatGetter {
-  size_t sizeBytes(const Blob& blob) const override {
-    const auto& tensor = blob.Get<TensorHIP>();
-    auto nbytes = tensor.nbytes();
-    if (nbytes > 0 && tensor.IsType<std::string>()) {
-      const auto* data = tensor.data<std::string>();
-      for (int i = 0; i < tensor.size(); ++i) {
-        nbytes += data[i].size();
-      }
-    }
-    return nbytes;
-  }
-};
-REGISTER_BLOB_STAT_GETTER(TensorHIP, TensorHIPStatGetter);
 } // namespace
 
 /**