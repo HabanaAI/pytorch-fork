pytorch/pytorch

commit 09a676f639b422baf947768c47116b944470e411

Add hooks for register_buffer/module/parameter (#86148)
As described in the issue, this PR adds hooks to be run when `register_parameter`, `register_buffer` and `register_module` are called.

Fixes #85837

cc @albanD @mruberry @jbschlosser @walterddr @kshitij12345 @saketh-are
Pull Request resolved: https://github.com/pytorch/pytorch/pull/86148
Approved by: https://github.com/albanD

torch/nn/modules/module.py
@@ -13,7 +13,8 @@ from typing import Union, Tuple, Any, Callable, Iterator, Set, Optional, overloa
 from ...utils.hooks import RemovableHandle
 
 __all__ = ['register_module_forward_pre_hook', 'register_module_forward_hook', 'register_module_backward_hook',
-           'register_module_full_backward_hook', 'Module']
+           'register_module_full_backward_hook', 'register_module_buffer_registration_hook',
+           'register_module_module_registration_hook', 'register_module_parameter_registration_hook', 'Module']
 
 _grad_t = Union[Tuple[Tensor, ...], Tensor]
 # See https://mypy.readthedocs.io/en/latest/generics.html#generic-methods-and-generic-self for the use
@@ -42,6 +43,11 @@ def _addindent(s_, numSpaces):
     s = first + '\n' + s
     return s
 
+r"""This tracks hooks common to all modules that are executed immediately before
+.registering the buffer/module/parameter"""
+_global_buffer_registration_hooks: Dict[int, Callable] = OrderedDict()
+_global_module_registration_hooks: Dict[int, Callable] = OrderedDict()
+_global_parameter_registration_hooks: Dict[int, Callable] = OrderedDict()
 
 class _WrappedHook:
     def __init__(self, hook: Callable, module: Optional["Module"] = None):
@@ -88,6 +94,78 @@ _global_forward_pre_hooks: Dict[int, Callable] = OrderedDict()
 _global_forward_hooks: Dict[int, Callable] = OrderedDict()
 
 _EXTRA_STATE_KEY_SUFFIX = '_extra_state'
+
+
+def register_module_buffer_registration_hook(hook: Callable[..., None]) -> RemovableHandle:
+    r"""Registers a buffer registration hook common to all modules.
+
+    .. warning ::
+
+        This adds global state to the `nn.Module` module
+
+    The hook will be called every time :func:`register_buffer` is invoked.
+    It should have the following signature::
+
+        hook(module, name, buffer) -> None or new buffer
+
+    The hook can modify the input or return a single modified value in the hook.
+
+    Returns:
+        :class:`torch.utils.hooks.RemovableHandle`:
+            a handle that can be used to remove the added hook by calling
+            ``handle.remove()``
+    """
+    handle = hooks.RemovableHandle(_global_buffer_registration_hooks)
+    _global_buffer_registration_hooks[handle.id] = hook
+    return handle
+
+
+def register_module_module_registration_hook(hook: Callable[..., None]) -> RemovableHandle:
+    r"""Registers a module registration hook common to all modules.
+
+    .. warning ::
+
+        This adds global state to the `nn.Module` module
+
+    The hook will be called every time :func:`register_module` is invoked.
+    It should have the following signature::
+
+        hook(module, name, submodule) -> None or new submodule
+
+    The hook can modify the input or return a single modified value in the hook.
+
+    Returns:
+        :class:`torch.utils.hooks.RemovableHandle`:
+            a handle that can be used to remove the added hook by calling
+            ``handle.remove()``
+    """
+    handle = hooks.RemovableHandle(_global_module_registration_hooks)
+    _global_module_registration_hooks[handle.id] = hook
+    return handle
+
+
+def register_module_parameter_registration_hook(hook: Callable[..., None]) -> RemovableHandle:
+    r"""Registers a parameter registration hook common to all modules.
+
+    .. warning ::
+
+        This adds global state to the `nn.Module` module
+
+    The hook will be called every time :func:`register_parameter` is invoked.
+    It should have the following signature::
+
+        hook(module, name, param) -> None or new parameter
+
+    The hook can modify the input or return a single modified value in the hook.
+
+    Returns:
+        :class:`torch.utils.hooks.RemovableHandle`:
+            a handle that can be used to remove the added hook by calling
+            ``handle.remove()``
+    """
+    handle = hooks.RemovableHandle(_global_parameter_registration_hooks)
+    _global_parameter_registration_hooks[handle.id] = hook
+    return handle
 
 
 def register_module_forward_pre_hook(hook: Callable[..., None]) -> RemovableHandle:
@@ -380,6 +458,8 @@ class Module:
                             "(torch Tensor or None required)"
                             .format(torch.typename(tensor), name))
         else:
+            for hook in _global_buffer_registration_hooks.values():
+                tensor = hook(self, name, tensor) or tensor
             self._buffers[name] = tensor
             if persistent:
                 self._non_persistent_buffers_set.discard(name)
@@ -426,6 +506,8 @@ class Module:
                 "as a function of another Tensor, compute the value in "
                 "the forward() method.".format(name))
         else:
+            for hook in _global_parameter_registration_hooks.values():
+                param = hook(self, name, param) or param
             self._parameters[name] = param
 
     def add_module(self, name: str, module: Optional['Module']) -> None:
@@ -450,6 +532,8 @@ class Module:
             raise KeyError("module name can't contain \".\", got: {}".format(name))
         elif name == '':
             raise KeyError("module name can't be empty string \"\"")
+        for hook in _global_module_registration_hooks.values():
+            module = hook(self, name, module) or module
         self._modules[name] = module
 
     def register_module(self, name: str, module: Optional['Module']) -> None:
@@ -1294,12 +1378,16 @@ class Module:
                     raise AttributeError(
                         "cannot assign module before Module.__init__() call")
                 remove_from(self.__dict__, self._parameters, self._buffers, self._non_persistent_buffers_set)
+                for hook in _global_module_registration_hooks.values():
+                    value = hook(self, name, value) or value
                 modules[name] = value
             elif modules is not None and name in modules:
                 if value is not None:
                     raise TypeError("cannot assign '{}' as child module '{}' "
                                     "(torch.nn.Module or None expected)"
                                     .format(torch.typename(value), name))
+                for hook in _global_module_registration_hooks.values():
+                    value = hook(self, name, value) or value
                 modules[name] = value
             else:
                 buffers = self.__dict__.get('_buffers')
@@ -1308,6 +1396,8 @@ class Module:
                         raise TypeError("cannot assign '{}' as buffer '{}' "
                                         "(torch.Tensor or None expected)"
                                         .format(torch.typename(value), name))
+                    for hook in _global_buffer_registration_hooks.values():
+                        value = hook(self, name, value) or value
                     buffers[name] = value
                 else:
                     super().__setattr__(name, value)

test/test_nn.py
@@ -591,6 +591,46 @@ class TestNN(NNTestCase):
         expected_grad = -sig_x * (1 - sig_x) * 2 * mask
         self.assertEqual(input.grad, expected_grad)
 
+    def test_hook_buffer_registration(self):
+        def buffer_registration_hook(module, name, buffer):
+            buffer.registered = True
+        handle = torch.nn.modules.module.register_module_buffer_registration_hook(
+            buffer_registration_hook
+        )
+        try:
+            l, n, s = self._create_basic_net()
+            for b in s.buffers():
+                self.assertTrue(getattr(b, "registered", False))
+        finally:
+            handle.remove()
+
+    def test_hook_submodule_registration(self):
+        def module_registration_hook(module, name, submodule):
+            module.registered = True
+            submodule.registered = True
+        handle = torch.nn.modules.module.register_module_module_registration_hook(
+            module_registration_hook
+        )
+        try:
+            l, n, s = self._create_basic_net()
+            for m in s.modules():
+                self.assertTrue(getattr(m, "registered", False))
+        finally:
+            handle.remove()
+
+    def test_hook_parameter_registration(self):
+        def parameter_registration_hook(module, name, parameter):
+            parameter.registered = True
+        handle = torch.nn.modules.module.register_module_parameter_registration_hook(
+            parameter_registration_hook
+        )
+        try:
+            l, n, s = self._create_basic_net()
+            for p in s.parameters():
+                self.assertTrue(getattr(p, "registered", False))
+        finally:
+            handle.remove()
+
     def test_to(self):
         m = nn.Linear(3, 5)
         self.assertIs(m, m.to('cpu'))

HabanaAI/pytorch-fork

commit 10d986b09b07ccbbd32db9892c9dd1dc457b183e

Fix registration hooks (#87369)
There is a bug in the implementation of the registration hooks introduced in https://github.com/pytorch/pytorch/pull/86148 whereby if the hook returns a tensor, then the short circuiting logic:
```
value = hook(self, name, value) or value
```
Raises an exception
```
RuntimeError: Boolean value of Tensor with more than one value is ambiguous
```
Fixing the logic so that it only checks to see if the value is `None` before overriding

Fixes #85837

CC: @albanD @jbschlosser
Pull Request resolved: https://github.com/pytorch/pytorch/pull/87369
Approved by: https://github.com/albanD

torch/nn/modules/module.py
@@ -500,7 +500,9 @@ class Module:
                             .format(torch.typename(tensor), name))
         else:
             for hook in _global_buffer_registration_hooks.values():
-                tensor = hook(self, name, tensor) or tensor
+                output = hook(self, name, tensor)
+                if output is not None:
+                    tensor = output
             self._buffers[name] = tensor
             if persistent:
                 self._non_persistent_buffers_set.discard(name)
@@ -548,7 +550,9 @@ class Module:
                 "the forward() method.".format(name))
         else:
             for hook in _global_parameter_registration_hooks.values():
-                param = hook(self, name, param) or param
+                output = hook(self, name, param)
+                if output is not None:
+                    param = output
             self._parameters[name] = param
 
     def add_module(self, name: str, module: Optional['Module']) -> None:
@@ -574,7 +578,9 @@ class Module:
         elif name == '':
             raise KeyError("module name can't be empty string \"\"")
         for hook in _global_module_registration_hooks.values():
-            module = hook(self, name, module) or module
+            output = hook(self, name, module)
+            if output is not None:
+                module = output
         self._modules[name] = module
 
     def register_module(self, name: str, module: Optional['Module']) -> None:
@@ -1468,7 +1474,9 @@ class Module:
                         "cannot assign module before Module.__init__() call")
                 remove_from(self.__dict__, self._parameters, self._buffers, self._non_persistent_buffers_set)
                 for hook in _global_module_registration_hooks.values():
-                    value = hook(self, name, value) or value
+                    output = hook(self, name, value)
+                    if output is not None:
+                        value = output
                 modules[name] = value
             elif modules is not None and name in modules:
                 if value is not None:
@@ -1476,7 +1484,9 @@ class Module:
                                     "(torch.nn.Module or None expected)"
                                     .format(torch.typename(value), name))
                 for hook in _global_module_registration_hooks.values():
-                    value = hook(self, name, value) or value
+                    output = hook(self, name, value)
+                    if output is not None:
+                        value = output
                 modules[name] = value
             else:
                 buffers = self.__dict__.get('_buffers')
@@ -1486,7 +1496,9 @@ class Module:
                                         "(torch.Tensor or None expected)"
                                         .format(torch.typename(value), name))
                     for hook in _global_buffer_registration_hooks.values():
-                        value = hook(self, name, value) or value
+                        output = hook(self, name, value)
+                        if output is not None:
+                            value = output
                     buffers[name] = value
                 else:
                     super().__setattr__(name, value)

test/test_nn.py
@@ -646,44 +646,53 @@ class TestNN(NNTestCase):
         self.assertEqual(input.grad, expected_grad)
 
     def test_hook_buffer_registration(self):
-        def buffer_registration_hook(module, name, buffer):
-            buffer.registered = True
-        handle = torch.nn.modules.module.register_module_buffer_registration_hook(
-            buffer_registration_hook
-        )
-        try:
-            l, n, s = self._create_basic_net()
-            for b in s.buffers():
-                self.assertTrue(getattr(b, "registered", False))
-        finally:
-            handle.remove()
+        for return_buffer in (True, False):
+            def buffer_registration_hook(module, name, buffer):
+                buffer.registered = True
+                if return_buffer:
+                    return buffer
+            handle = torch.nn.modules.module.register_module_buffer_registration_hook(
+                buffer_registration_hook
+            )
+            try:
+                l, n, s = self._create_basic_net()
+                for b in s.buffers():
+                    self.assertTrue(getattr(b, "registered", False))
+            finally:
+                handle.remove()
 
     def test_hook_submodule_registration(self):
-        def module_registration_hook(module, name, submodule):
-            module.registered = True
-            submodule.registered = True
-        handle = torch.nn.modules.module.register_module_module_registration_hook(
-            module_registration_hook
-        )
-        try:
-            l, n, s = self._create_basic_net()
-            for m in s.modules():
-                self.assertTrue(getattr(m, "registered", False))
-        finally:
-            handle.remove()
+        for return_submodule in (True, False):
+            def module_registration_hook(module, name, submodule):
+                module.registered = True
+                submodule.registered = True
+                if return_submodule:
+                    return submodule
+            handle = torch.nn.modules.module.register_module_module_registration_hook(
+                module_registration_hook
+            )
+            try:
+                l, n, s = self._create_basic_net()
+                for m in s.modules():
+                    self.assertTrue(getattr(m, "registered", False))
+            finally:
+                handle.remove()
 
     def test_hook_parameter_registration(self):
-        def parameter_registration_hook(module, name, parameter):
-            parameter.registered = True
-        handle = torch.nn.modules.module.register_module_parameter_registration_hook(
-            parameter_registration_hook
-        )
-        try:
-            l, n, s = self._create_basic_net()
-            for p in s.parameters():
-                self.assertTrue(getattr(p, "registered", False))
-        finally:
-            handle.remove()
+        for return_parameter in (True, False):
+            def parameter_registration_hook(module, name, parameter):
+                parameter.registered = True
+                if return_parameter:
+                    return parameter
+            handle = torch.nn.modules.module.register_module_parameter_registration_hook(
+                parameter_registration_hook
+            )
+            try:
+                l, n, s = self._create_basic_net()
+                for p in s.parameters():
+                    self.assertTrue(getattr(p, "registered", False))
+            finally:
+                handle.remove()
 
     def test_to(self):
         m = nn.Linear(3, 5)