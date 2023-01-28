HabanaAI/pytorch-fork

commit a50ae30820b1c58b105c5fd23cc87744aa7b69a1

Fix CUDA error not getting captured by handler (#92227)
Fixes #91758. Still leaves functions on the hotpath.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/92227
Approved by: https://github.com/ngimel, https://github.com/malfet

c10/cuda/CUDAException.cpp
@@ -10,11 +10,12 @@ namespace c10 {
 namespace cuda {
 
 void c10_cuda_check_implementation(
+    const int32_t err,
     const char* filename,
     const char* function_name,
     const int line_number,
     const bool include_device_assertions) {
-  const auto cuda_error = cudaGetLastError();
+  const auto cuda_error = static_cast<cudaError_t>(err);
   const auto cuda_kernel_failure = include_device_assertions
       ? c10::cuda::CUDAKernelLaunchRegistry::get_singleton_ref().has_failed()
       : false;

c10/cuda/CUDADeviceAssertionHost.cpp
@@ -13,8 +13,17 @@
 #include <string>
 #include <thread>
 
-#define CHECK_CUDA_API_CALL_WITHOUT_CHECKING_DEVICE_ASSERTS() \
-  c10_cuda_check_implementation(__FILE__, __FUNCTION__, __LINE__, false)
+#define C10_CUDA_CHECK_WO_DSA(EXPR)                                 \
+  do {                                                              \
+    const cudaError_t __err = EXPR;                                 \
+    c10::cuda::c10_cuda_check_implementation(                       \
+        static_cast<int32_t>(__err),                                \
+        __FILE__,                                                   \
+        __func__, /* Line number data type not well-defined between \
+                      compilers, so we perform an explicit cast */  \
+        static_cast<uint32_t>(__LINE__),                            \
+        false);                                                     \
+  } while (0)
 
 namespace c10 {
 namespace cuda {
@@ -27,8 +36,7 @@ namespace {
 /// an infinite initialization loop for CUDAKernelLaunchRegistry
 int dsa_get_device_id() {
   int device = -1;
-  C10_CUDA_ERROR_HANDLED(cudaGetDevice(&device));
-  CHECK_CUDA_API_CALL_WITHOUT_CHECKING_DEVICE_ASSERTS();
+  C10_CUDA_CHECK_WO_DSA(cudaGetDevice(&device));
   return device;
 }
 
@@ -41,9 +49,8 @@ int dsa_get_device_id() {
 /// an infinite initialization loop for CUDAKernelLaunchRegistry
 int dsa_get_device_compute_capability(const int device_num) {
   int compute_capability = -1;
-  C10_CUDA_ERROR_HANDLED(cudaDeviceGetAttribute(
+  C10_CUDA_CHECK_WO_DSA(cudaDeviceGetAttribute(
       &compute_capability, cudaDevAttrComputeCapabilityMajor, device_num));
-  CHECK_CUDA_API_CALL_WITHOUT_CHECKING_DEVICE_ASSERTS();
   return compute_capability;
 }
 #endif
@@ -53,8 +60,7 @@ int dsa_get_device_compute_capability(const int device_num) {
 /// an infinite initialization loop for CUDAKernelLaunchRegistry
 int dsa_get_device_count() {
   int device_count = -1;
-  C10_CUDA_ERROR_HANDLED(cudaGetDeviceCount(&device_count));
-  CHECK_CUDA_API_CALL_WITHOUT_CHECKING_DEVICE_ASSERTS();
+  C10_CUDA_CHECK_WO_DSA(cudaGetDeviceCount(&device_count));
   return device_count;
 }
 
@@ -288,25 +294,22 @@ DeviceAssertionsData* CUDAKernelLaunchRegistry::
   // system
   DeviceAssertionsData* uvm_assertions_ptr = nullptr;
 
-  C10_CUDA_ERROR_HANDLED(
+  C10_CUDA_CHECK_WO_DSA(
       cudaMallocManaged(&uvm_assertions_ptr, sizeof(DeviceAssertionsData)));
-  CHECK_CUDA_API_CALL_WITHOUT_CHECKING_DEVICE_ASSERTS();
 
-  C10_CUDA_ERROR_HANDLED(cudaMemAdvise(
+  C10_CUDA_CHECK_WO_DSA(cudaMemAdvise(
       uvm_assertions_ptr,
       sizeof(DeviceAssertionsData),
       cudaMemAdviseSetPreferredLocation,
       cudaCpuDeviceId));
-  CHECK_CUDA_API_CALL_WITHOUT_CHECKING_DEVICE_ASSERTS();
 
   // GPU will establish direct mapping of data in CPU memory, no page faults
   // will be generated
-  C10_CUDA_ERROR_HANDLED(cudaMemAdvise(
+  C10_CUDA_CHECK_WO_DSA(cudaMemAdvise(
       uvm_assertions_ptr,
       sizeof(DeviceAssertionsData),
       cudaMemAdviseSetAccessedBy,
       cudaCpuDeviceId));
-  CHECK_CUDA_API_CALL_WITHOUT_CHECKING_DEVICE_ASSERTS();
 
   // Initialize the memory from the CPU; otherwise, pages may have to be created
   // on demand. We think that UVM documentation indicates that first access may

c10/cuda/CUDAException.h
@@ -24,17 +24,16 @@ class C10_CUDA_API CUDAError : public c10::Error {
 };
 } // namespace c10
 
-#define C10_CUDA_CHECK(EXPR)                                             \
-  do {                                                                   \
-    /* We get & disarm the error inside of */                            \
-    /* `c10_cuda_check_implementation` */                                \
-    C10_UNUSED const cudaError_t __err = EXPR;                           \
-    c10::cuda::c10_cuda_check_implementation(                            \
-        __FILE__,                                                        \
-        __func__, /* Line number's data type is not well-defined between \
-                      compilers, so we perform an explicit cast */       \
-        static_cast<uint32_t>(__LINE__),                                 \
-        true);                                                           \
+#define C10_CUDA_CHECK(EXPR)                                        \
+  do {                                                              \
+    const cudaError_t __err = EXPR;                                 \
+    c10::cuda::c10_cuda_check_implementation(                       \
+        static_cast<int32_t>(__err),                                \
+        __FILE__,                                                   \
+        __func__, /* Line number data type not well-defined between \
+                      compilers, so we perform an explicit cast */  \
+        static_cast<uint32_t>(__LINE__),                            \
+        true);                                                      \
   } while (0)
 
 #define C10_CUDA_CHECK_WARN(EXPR)                              \
@@ -93,6 +92,7 @@ namespace cuda {
 /// In the event of a CUDA failure, formats a nice error message about that
 /// failure and also checks for device-side assertion failures
 C10_CUDA_API void c10_cuda_check_implementation(
+    const int32_t err,
     const char* filename,
     const char* function_name,
     const int line_number,

commit 0c6f7f70473672718c11d55ab044433086a0d67f

Fix C10_CUDA_CHECK for failing to capture last cuda error occasionally  (#93192)
Fix C10_CUDA_CHECK for failing to capture last cuda error occasionally

This error was accidentally introduced by #92227, which was trying to fix_ #91758 as introduced in #85256.

The unit test `TestCuda.test_events_multi_gpu_elapsed_time` has been failed since that PR got merged (in cuda 11.8 and cuda 12.0). That test requires >=2 GPU, so it's probably not tested in the OSS CI?
```
python test/test_cuda.py -v -k TestCuda.test_events_multi_gpu_elapsed_time
```

E.g. in https://github.com/pytorch/pytorch/actions/runs/4026926691/jobs/6922406192
```
2023-01-27T19:41:32.2312162Z   test_events_multi_gpu_elapsed_time (__main__.TestCuda) ... skip: detected only one GPU (0.001s)
```

The original C10_CUDA_CHECK before #85256 has an extra `cudaGetLastError` that captures those cuda errors, https://github.com/pytorch/pytorch/pull/85256/files#diff-0823e63e781acf56e93a5553ed7feee0db0bda05d86e2560c7b80e87e32e0024L41-L42

This extra `cudaGetLastError` was originally introduced in #17337. As commented here https://github.com/pytorch/pytorch/pull/17337/files#r259104503

> soumith on Feb 21, 2019:
Without this, a previously raised error was still lingering and falsely being triggered for a subsequent CUDA call. colesbury suggested that this is the right thing to do.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/93192
Approved by: https://github.com/ezyang

c10/cuda/CUDAException.cpp
@@ -24,6 +24,9 @@ void c10_cuda_check_implementation(
     return;
   }
 
+  auto error_unused C10_UNUSED = cudaGetLastError();
+  (void)error_unused;
+
   std::string check_message;
 #ifndef STRIP_ERROR_MESSAGES
   check_message.append("CUDA error: ");