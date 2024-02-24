pytorch/pytorch

commit 2410848df3731a213fa9d1ecf2d4fa6a1c4b42bd

[dynamo] Use EQUALS_MATCH guard for mod.training (#120147)
Pull Request resolved: https://github.com/pytorch/pytorch/pull/120147
Approved by: https://github.com/jansel
ghstack dependencies: #120132, #120140, #120145

torch/_dynamo/guards.py
@@ -463,10 +463,7 @@ class GuardBuilder(GuardBuilderBase):
 
         def setup_guard():
             assert istype(val.training, bool)
-            # TODO: Why doesn't this use produce_guard_code?
-            self.code.append(
-                GuardCodeList([f"{ref}.training == {val.training}"], guard)
-            )
+            self._guard_on_attribute(guard, "training", GuardBuilder.EQUALS_MATCH)
 
         if hasattr(val, "training"):
             # There are cases where a monkeypatched object has a guard made between __new__ and __init__

HabanaAI/pytorch-fork

commit 60c33efd923b9840923d6569bda6705887e4bfa0

[dynamo] add stronger test for dynamo memory leaks (#120459)
This issue was raised by a regression of https://github.com/pytorch/pytorch/issues/112090 caused by https://github.com/pytorch/pytorch/pull/120147.

Make the memory leak test stronger by using weakref to check for model deletion instead of measuring CUDA memory allocation.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/120459
Approved by: https://github.com/jansel

test/dynamo/test_dynamic_shapes.py
@@ -85,6 +85,11 @@ if TEST_Z3:
         DynamicShapesReproTests.test_dynamic_shapes_float_guard_dynamic_shapes  # noqa: F821
     )
 
+    # TODO model is somehow not being freed when z3 is available
+    unittest.expectedFailure(
+        DynamicShapesMiscTests.test_custom_module_free_dynamic_shapes  # noqa: F821
+    )
+
 unittest.expectedFailure(
     # Test is only valid without dynamic shapes
     DynamicShapesReproTests.test_many_views_with_mutation_dynamic_shapes  # noqa: F821

test/dynamo/test_misc.py
@@ -6,6 +6,7 @@ import dataclasses
 import dis
 import enum
 import functools
+import gc
 import itertools
 import logging
 import math
@@ -9528,43 +9529,46 @@ fn
 
         self.assertEqual(fn(torch.tensor([0])), torch.zeros(0))
 
-    @unittest.skipIf(not TEST_CUDA, "requires cuda")
-    def test_module_free(self):
-        """Test that CUDA memory is freed when a model goes out of scope"""
+    def _test_compile_model_free(self, model_inp_ctr, weakref_watch):
+        """
+        Args:
+        model_inp_ctr
+            - constructor that returns a new model and inputs to that model
+        weakref_watch
+            - function that returns a layer of the model for weakref to
+              finalize on, so we can check that the layer is freed after
+              the model goes out of scope
+        """
+        cleared = False
+
+        def finalize():
+            nonlocal cleared
+            cleared = True
+
+        def run():
+            mod, inp = model_inp_ctr()
+            weakref.finalize(weakref_watch(mod), finalize)
+            torch.compile(mod, backend="eager")(inp)
+
+        run()
+        gc.collect()
+        self.assertTrue(cleared)
+
+    def test_custom_module_free(self):
+        """Test that a model is freed when it goes out of scope"""
 
         class Mod(torch.nn.Module):
             def __init__(self):
                 super(Mod, self).__init__()
-                self.fc = torch.nn.Linear(10000, 10000)
+                self.fc = torch.nn.Linear(100, 100)
 
             def forward(self, out):
                 return self.fc(out)
 
-        def run(compile):
-            mod = Mod().cuda()
-            if compile:
-                mod = torch.compile(mod, backend="eager")
-            inp = torch.rand(10000, 10000).cuda()
-            mod(inp)
-
-        def clean_and_report_memory():
-            import gc
-
-            gc.collect()
-            return torch.cuda.memory_allocated()
-
-        run(False)
-        # mem1 = clean_and_report_memory()
-        run(True)
-        mem2 = clean_and_report_memory()
-        torch._dynamo.reset_code_caches()
-        mem3 = clean_and_report_memory()
-
-        # it's possible for dynamo to hold on to more memory
-        # even after a _dynamo.reset[_code_caches], so we omit the following check.
-        # self.assertEqual(mem1, mem2)
-
-        self.assertEqual(mem2, mem3)
+        self._test_compile_model_free(
+            lambda: (Mod(), torch.randn(100, 100)),
+            lambda mod: mod.fc,
+        )
 
     def test_dynamo_cache_move_to_front(self):
         class Mod(torch.nn.Module):