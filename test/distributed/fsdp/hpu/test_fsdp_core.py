4fcaf7d7aead668e07fcfe0fc32bbba351a3c885
internal developer <developer@habana.ai>
2024-02-26 13:08:38 +0200

    fsdp files for PTv2.2.0
    Change-Id: I504d22769b6be7afe68e5b6582c1b4211abb69b7

--- test/distributed/fsdp/hpu/test_fsdp_core.py
+++ test/distributed/fsdp/hpu/test_fsdp_core.py
@@ -11,7 +11,7 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn
 from torch.distributed.fsdp import CPUOffload, MixedPrecision
-from torch.distributed.fsdp.flat_param import FlatParamHandle
+from torch.distributed.fsdp._flat_param import FlatParamHandle
 from torch.distributed.fsdp.fully_sharded_data_parallel import (
     BackwardPrefetch,
     FullyShardedDataParallel as FSDP,
@@ -338,7 +338,7 @@ class TestHooks(FSDPTest):
     def _test_pre_backward_hook_registration(self, model):
         optim = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
         optim.zero_grad()
-        # Inputs always cuda, as computation happes on CUDA device only
+        # Inputs always cuda, as computation happens on CUDA device only
         input = model.module.get_input(torch.device("hpu"))
         output = model(*input)
         # this is pre-bwd hook
