8eb2661164f304a666987aae6056c8f9c3411ee5
internal developer <developer@habana.ai>
2024-07-30 07:37:22 +0300

    Add support for other backends in get_preferred_device
    Currenlty get_preferred_device supports only cuda and cpu.
    Add support for other backends using backend config.
    
    PR: https://github.com/pytorch/pytorch/pull/132118
    
    Change-Id: Ia92c27b33b4d37d09c5238bc8d946d677610de46
    Signed-off-by: internal developer <developer@habana.ai>

--- torch/distributed/_shard/sharded_tensor/api.py
+++ torch/distributed/_shard/sharded_tensor/api.py
@@ -16,6 +16,7 @@ import torch.distributed as dist
 import torch.distributed._shard.sharding_spec as shard_spec
 from torch.distributed import distributed_c10d, rpc
 from torch.distributed._shard._utils import DEPRECATE_MSG
+from torch._utils import _get_device_module
 from torch.distributed._shard.sharding_spec._internals import (
     check_tensor,
     validate_non_overlapping_shards_metadata,
@@ -370,6 +371,17 @@ class ShardedTensor(ShardedTensorBase):
         Return the preferred device to be used when creating tensors for collectives.
         This method takes into account the associated process group
         """
+        backend = dist.get_backend(self._process_group)
+        backend_config = dist.BackendConfig(dist.get_backend(self._process_group))
+        if backend == dist.Backend.NCCL:
+            return torch.device(torch.cuda.current_device())
+        else:
+            for device, backend_str in backend_config.get_device_backend_map().items():
+                if backend_str == backend and device != "cpu":
+                    return torch.device(
+                        device, _get_device_module(device).current_device()
+                    )
+        return torch.device("cpu")
         if dist.get_backend(self._process_group) == dist.Backend.NCCL:
             return torch.device(torch.cuda.current_device())
         return torch.device("cpu")
