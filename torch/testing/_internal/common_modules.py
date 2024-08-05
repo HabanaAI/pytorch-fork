d2b2e8d7f3db22879fa60183ec6fd21cd6f0874e
internal developer <developer@habana.ai>
2024-08-05 15:56:08 +0300

    Rectify syntax error
    Change-Id: I84c93ca7f99568acdad26a879d1439a460fe5f36

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -215,7 +215,7 @@ class ModuleInfo:
                  decorators=None,  # Additional decorators to apply to generated tests
                  dtypes=floating_types(),  # dtypes this function is expected to work with
                  dtypesIfMPS=(torch.float16, torch.float32,),  # dtypes this function is expected to work with on MPS
-                 dtypesIfHpu=(torch.bfloat16,torch.float32,)
+                 dtypesIfHpu=(torch.bfloat16,torch.float32,),
                  supports_gradgrad=True,  # whether the op supports second order gradients
                  gradcheck_nondet_tol=0.0,  # tolerance for nondeterminism while performing gradcheck
                  module_memformat_affects_out=False,  # whether converting module to channels last will generate
