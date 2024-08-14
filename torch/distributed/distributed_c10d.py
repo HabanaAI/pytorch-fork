d9ba83de9af0a6ab6b1309e0f2418e061f31c9ee
internal developer <developer@habana.ai>
2024-08-14 13:41:30 +0300

    Add colease support for devicetype other than cuda
    Upstream PR: https://github.com/pytorch/pytorch/pull/133429
    
    Change-Id: I4bf4f169a0d80ba19a6055ddd9d0eb647b501e23

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -2230,6 +2230,14 @@ class _CoalescingManager:
         for work in self.works:
             work.wait()
 
+# Global set to store coalescing supported device types
+COALESING_SUPPORTED_DTYPES = {"cuda", "hpu"}
+
+def has_coalescing_manager(device_type):
+    """
+    Checks if the given device type has a coalescing feature support.
+    """
+    return device_type in COALESING_SUPPORTED_DTYPES
 
 @contextlib.contextmanager
 def _coalescing_manager(
@@ -2365,7 +2373,7 @@ def batch_isend_irecv(p2p_op_list):
     _check_p2p_op_list(p2p_op_list)
     group = p2p_op_list[0].group
     device = p2p_op_list[0].tensor.device
-    if device.type == "cuda":
+    if has_coalescing_manager(device.type):
         # NCCL style coalescing
         with _coalescing_manager(group, device, async_ops=True) as cm:
             for p2p_op in p2p_op_list:
