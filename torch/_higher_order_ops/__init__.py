4f00c5ae43ce9b4a9baabd07eef09a2970e9614d
internal developer <developer@habana.ai>
2024-08-22 05:06:15 +0300

    add new HOO hints_wrapper to annotate function with hints
    Cherry-pick changes from upstream PR: https://github.com/pytorch/pytorch/pull/132860
    
    Change-Id: I540d5a17534b8166a1a2ca49a8a96f475ee56a96

--- torch/_higher_order_ops/__init__.py
+++ torch/_higher_order_ops/__init__.py
@@ -1,3 +1,4 @@
 from .cond import cond
 from .while_loop import while_loop
 from .flex_attention import flex_attention, flex_attention_backward
+from torch._higher_order_ops.hints_wrap import hints_wrapper
