97f4c282c45c03862b4234e4c695b1c90a06b996
internal developer <developer@habana.ai>
2024-09-02 04:55:11 +0300

    Trace fwd graph under no_grad mode
    Cherry-pick: https://github.com/pytorch/pytorch/pull/134872
    
    Change-Id: I382a8317e175cc25f6cc95aaebe10e2e0d49ee18

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -1783,6 +1783,7 @@ class AutogradFunctionApplyVariable(VariableTracker):
             fwd_args,
             kwargs,
             "autograd.Function",
+            enable_grad=False,
             set_subgraph_inputs="semi_automatic",
             restore_side_effects=False,
             tracer=fwd_tracer,
