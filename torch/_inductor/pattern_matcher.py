cf9104969fe39e83f21dabfc6981f76a70557402
internal developer <developer@habana.ai>
2025-02-04 08:35:54 +0530

    Added getitem support for inductor pattern_matcher (#44)
    upstream PR: https://github.com/pytorch/pytorch/pull/144980
    
    Change-Id: Id57d95bdabc27563ab5450a868314fcf4adf837e

--- torch/_inductor/pattern_matcher.py
+++ torch/_inductor/pattern_matcher.py
@@ -499,6 +499,8 @@ class _TargetExpr(PatternExpr):
             return f"torch.{first_repr}"
         elif isinstance(self.fns[0], torch._ops.OpOverload):
             return str(self.fns[0])
+        elif self.fns[0] is getattr(operator, first_repr, None):
+            return f"operator.{first_repr}"
         else:
             return first_repr
 
@@ -1432,6 +1434,7 @@ def _serialize_pattern(
             {msg}
             import torch
             import torch._inductor
+            import operator
 
             aten = torch.ops.aten
             prims = torch.ops.prims
